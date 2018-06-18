e1390ba5bb82ecb8d2fc1e69429649ae36ef16c5
kougei-abe <g1727005@st.t-kougei.ac.jp>
2018-06-18 17:36:53 +0900

    ジャンプモーションの追加

--- ClimbCloud/Assets/PlayerController.cs
+++ ClimbCloud/Assets/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour {
         // ジャンプする
         if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
         {
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
@@ -51,7 +52,13 @@ public class PlayerController : MonoBehaviour {
         }
 
         // プレイヤの速度に応じてアニメーション速度を変える
-        this.animator.speed = speedx / 2.0f;
+        if(this.rigid2D.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else{
+            this.animator.speed = 1.0f;
+        }
     }
 
     // ゴールに到達
